207e906eac0a5caca558b1eb059ae27d868f5fcd
Vishal3990 <68455922+Vishal3990@users.noreply.github.com>
2024-03-14 18:42:47 +0530

    Add files via upload

---
+++ MultiLevel/src/com/java/vishal/Word.java
@@ -0,0 +1,26 @@
+package com.java.vishal;
+
+public class Word extends WordPad {
+private boolean spellCheckEnabled;
+
+public Word() {
+	super();
+	this.spellCheckEnabled = true;
+}
+
+public void spellCheck()
+{
+	if(spellCheckEnabled)
+	{
+		System.out.println("Spell check is enabled");
+	}else
+	{
+		System.out.println("Spell check is disabled");
+	}
+}
+public void enableSpellCheck(boolean enable)
+{
+	spellCheckEnabled=enable;
+}
+
+}
