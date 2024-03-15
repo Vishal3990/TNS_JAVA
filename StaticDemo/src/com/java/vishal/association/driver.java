3aba1c0290a900e9fa61e258bc3702518466b1cb
Vishal3990 <68455922+Vishal3990@users.noreply.github.com>
2024-03-15 20:15:16 +0530

    Add files via upload

---
+++ StaticDemo/src/com/java/vishal/association/driver.java
@@ -0,0 +1,13 @@
+package com.java.vishal.association;
+public class driver {
+
+	public static void main(String[] args) {
+		employee e1 = new employee(101,"Vishal");
+		System.out.println(e1);
+		
+		employee e2 = new employee(102,"Mohit");
+		System.out.println(e2);
+
+	}
+
+}
