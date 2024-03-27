cc441d1d2b26dbe4dca2bd53056e523dfe7e4b97
Vishal3990 <68455922+Vishal3990@users.noreply.github.com>
2024-03-27 18:20:03 +0530

    Add files via upload

---
+++ Abstractiondemo/src/com/java/vishal/abstractiondemo/Bank.java
@@ -0,0 +1,10 @@
+package com.java.vishal.abstractiondemo;
+public interface Bank {		
+		double minBalance = 10000;
+		double depositLimit = 25000;
+//		methods declared in interfces are static
+		void deposit(Account account,double amount);
+		
+		void withdraw(Account account,double amount);
+		
+	}
